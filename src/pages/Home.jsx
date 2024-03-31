cb0af87853f0150a6bc5758ea8e42201f9371cb6
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-31 14:01:01 -0700

    initialized project section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { Hero } from "../components/Hero";
 import { AboutMe } from "../components/AboutMe";
+import { Projects } from "../components/Projects";
 
 export const Home = () => {
     return(
         <div>
             <Hero />
             <AboutMe />
+            <Projects />
         </div>
     );
 }
