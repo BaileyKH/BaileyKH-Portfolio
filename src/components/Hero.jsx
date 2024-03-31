d273c3ca75861579b10e97034ec0bca5c4aa78a4
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-30 18:22:37 -0700

    hero component additions | nav ui adjustment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,29 @@
-import { IconBrandGithub } from '@tabler/icons-react';
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react';
+import img from '/src/assets/headshot.png'
+
 
 export const Hero = () => {
     return(
-        <div className="flex justify-center items-center h-screen mx-52 px-10">
-            <div className="w-2/3 flex flex-col">
-                <h1 className="text-6xl pb-5 font-bold">Front-End Developer</h1>
-                <p className="pb-5 leading-relaxed">Hello! I'm Bailey Henderson and I am Front-End React <span>addict</span> Developer currently based in Laveen, Arizona</p>
-                <div className="flex pb-5">
-                    <IconBrandGithub stroke={1} className="pr-4"/>
-                    <p>Linked-In</p>
+        <div className="flex justify-center items-center h-screen mx-48 px-10">
+            <div className="flex">
+                <div className="w-1/2 flex flex-col">
+                    <h1 className="text-6xl pb-5 font-bold">Front-End React Developer</h1>
+                    <p className="pb-5 leading-relaxed">Hello! I'm Bailey Henderson and I am Front-End React <span className="line-through">addict</span> Developer currently based in Laveen, Arizona 🌵</p>
+                    <div className="flex pb-5 justify-start items-center ">
+                        <a href="https://github.com/BaileyKH" target="_blank"><IconBrandGithub stroke={1} className="w-12 h-12 pr-1 cursor-pointer"/></a>
+                        <a><IconBrandLinkedin stroke={1} className="w-12 h-12" /></a>
+                    </div>
+                    <div className="flex items-start">
+                    <a href="./filepath/filename.pdf" download><button className="bg-primaryRed px-3 py-2 rounded-lg text-backgroundWhite">Download Resume</button></a>
+                    </div>
+                    <div className="mt-24">
+                        <h2 className="font-semibold ">Stack |</h2>
+                    </div>
                 </div>
-                <div className="flex items-start">
-                <button className="bg-primaryRed px-3 py-2 rounded-lg text-backgroundWhite">Download Resume</button>
+                <div className="w-1/2 flex justify-center items-start">
+                    <img src={img} className="rounded-3xl border-4 border-primaryRed"/>
                 </div>
             </div>
-            <div className="w-1/3">
-
-            </div>
-
         </div>
     );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
-
+import { IconSpider } from '@tabler/icons-react';
 
 export const NavBar = () => {
     return(
         <nav className="bg-white p-6 flex justify-between items-center shadow-md">
-           <h1><a href="#" className="text-4xl hover:text-primaryGray">BaileyKH</a></h1>
+            <div className='flex justify-between items-center'>
+                <h1><a href="#" className="text-4xl hover:text-primaryGray">BaileyKH</a></h1>
+                <IconSpider stroke={1} className="w-8 h-8"/>
+            </div>
             <div className="flex gap-6 justify-end items-center text-lg">
                 <a href="#">About Me</a>
                 <a href="#">Projects</a>
