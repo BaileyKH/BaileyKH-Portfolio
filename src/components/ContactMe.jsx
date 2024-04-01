ed6cef7b54332832795ec0c356ce6e6074ea8449
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-01 11:42:41 -0700

    contact content

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import { IconMail, IconBrandLinkedin } from '@tabler/icons-react';
+
 export const ContactMe = () => {
 
     const [time, setTime] = useState('');
@@ -25,12 +27,23 @@ export const ContactMe = () => {
 
     return(
         <div id="contact" className="bg-white">
-            <div className="flex flex-col px-12 py-20 drop-shadow-lg">
+            <div className="flex flex-col px-40 py-28 drop-shadow-lg w-1/2">
                 <div>
-                    <h2 className="text-primaryRed font-semibold">Contact Me!</h2>
-                    <p className="font-light">It is currently {time}, but please feel free to get in contact with me at your earliest convenience. I am always available!</p>
+                    <h2 className="text-primaryRed font-semibold pb-2">Contact Me!</h2>
+                    <p className="font-light">It is currently <span className="font-semibold">{time}</span>, but please feel free to get in contact with me at your earliest convenience. I am always available!</p>
                 </div>
-                <div></div>
+                <div className="flex mt-16">
+                    <div className="flex mr-8">
+                        <IconMail stroke={1} className="w-12 h-12 p-2 mr-4 bg-backgroundWhite rounded-full"/>
+                        <div className="flex flex-col">
+                            <p className="text-primaryRed">E-Mail</p>
+                            <a href="mailto:hen.baileyk@gmail.com" className="font-light">hen.baileyk@gmail.com</a>
+                        </div>
+                    </div>
+                    <div>
+                        <IconBrandLinkedin stroke={1} className="w-12 h-12 p-2 mr-4 bg-backgroundWhite rounded-full" />
+                    </div>
+                </div>
             </div>
         </div>
     );
