d6831b5ddbada3c0379052337ca538c2cdfe6b7d
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-01 14:22:29 -0700

    addition of project content

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -6,12 +6,16 @@ export const projects = [
         id: 1,
         title: "Stoked Snowboarding",
         img: Stoked,
-        description: "Stoked is a simple ecommerce website that allows users to browse for snowboard equipment. It allows for views of a product page and cart page, while also allowing to filter products by their designated category."
+        description: "Stoked is a simple ecommerce website that allows users to browse for snowboard equipment. It allows for views of a product page and cart page, while also allowing to filter products by their designated category.",
+        code: "https://github.com/BaileyKH/stoked-app",
+        demo: "https://stokedsnow.netlify.app/"
     },
     {
         id: 2,
         title: "Jammming",
         img: Jammming,
-        description: "Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account. "
+        description: "Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account. ",
+        code: "https://github.com/BaileyKH/Jammming",
+        demo: "https://playlistcreation.netlify.app/"
     },
 ]
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,40 @@
 import { projects } from "./ProjectData";
 
+import { IconExternalLink, IconCode } from '@tabler/icons-react';
+
 export const Projects = () => {
 
     const filteredProjects = projects.filter((project) => project.id <= 2);
 
     return(
         <div id="projects" className="flex flex-col">
-            <div className="mx-40 my-28">
+            <div className="mx-40 my-28 ">
                 <h2 className=" text-primaryRed font-semibold">Projects</h2>
                 <p>Elegant Code, Engaging Designs</p>
-                <div className="flex">
                 {filteredProjects.map((project) => (
-                    
-                ))}
+                <div className="flex py-10 justify-evenly items-center bg-white mt-8 pl-8 rounded-lg">
+                    <div className="w-1/2 flex justify-center items-center">
+                        <img src={project.img} className="w-fit h-auto rounded-lg"/>
+                    </div>
+                    <div className="w-1/2 flex flex-col justify-center items-center px-8">
+                        <h3 className="text-xl text-primaryRed font-semibold pb-4">{project.title}</h3>
+                        <p className="font-light text-center">{project.description}</p>
+                        <div className="flex justify-evenly mt-8">
+                            <div className="flex justify-center items-center bg-primaryGray text-white rounded-lg mx-2 py-1 px-2"><a href={project.demo} target="_blank" className="flex justify-center items-center">
+                                <IconExternalLink stroke={1} className="pr-1"/>
+                                <p className="font-extralight text-sm">Demo</p>
+                                </a>
+                            </div>
+                            <div className="flex bg-primaryGray text-white rounded-lg mx-2 py-1 px-2"><a href={project.code} target="_blank" className="flex justify-center items-center">
+                                <IconCode stroke={1} className="pr-1"/>
+                                <p className="font-extralight text-sm">Code</p>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                
                 </div>
+                ))}
             </div>
         </div>
     );
