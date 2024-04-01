9262b2356d5bcb02b65cbc8469ea12ab693c2d56
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-03-31 19:11:59 -0700

    added current time and small content to Contact

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,17 +1,36 @@
-
+import { useState, useEffect } from "react";
 
 export const ContactMe = () => {
+
+    const [time, setTime] = useState('');
+
+  useEffect(() => {
+    const updateArizonaTime = () => {
+      const currentDate = new Date();
+      const arizonaTime = currentDate.toLocaleTimeString('en-US', {
+        timeZone: 'America/Phoenix',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true, 
+      });
+      setTime(arizonaTime);
+    };
+
+    updateArizonaTime();
+
+    const intervalId = setInterval(updateArizonaTime, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
     return(
         <div id="contact" className="bg-white">
-            <div className="w-screen">
-                <div className="flex px-12 py-20">
-                    <div className="w-1/2">
-                        <h1>contact me</h1>
-                    </div>
-                    <div className="w-1/2 flex flex-col justify-center items-start px-8 ">
-                        <h1>contact me</h1>
-                    </div>
+            <div className="flex flex-col px-12 py-20 drop-shadow-lg">
+                <div>
+                    <h2 className="text-primaryRed font-semibold">Contact Me!</h2>
+                    <p className="font-light">It is currently {time}, but please feel free to get in contact with me at your earliest convenience. I am always available!</p>
                 </div>
+                <div></div>
             </div>
         </div>
     );
