28dbd4e6c11326f3f06dc8d163ecdad9ff5e6f26
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-04 16:12:50 -0700

    responsive nav update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { IconSpider } from '@tabler/icons-react';
 export const NavBar = () => {
     
     return(
-        <nav className="bg-white w-screen p-6 flex justify-between items-center shadow-md fixed z-10 top-0">
+        <nav className="bg-white w-full p-6 flex justify-between items-center shadow-md fixed z-10 top-0 ">
             <div className='flex justify-between items-center'>
                 <h1><a href="#" className="text-4xl hover:text-primaryGray">BaileyKH</a></h1>
                 <IconSpider stroke={1} className="w-8 h-8"/>
