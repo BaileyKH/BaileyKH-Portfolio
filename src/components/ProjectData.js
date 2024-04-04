a0974da2a79e9e7eac885fb496e0fcb5305e034f
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-04 13:25:10 -0700

    project redirect fix

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -8,7 +8,7 @@ export const projects = [
         img: Stoked,
         description: "Stoked is a simple ecommerce website that allows users to browse for snowboard equipment. It allows for views of a product page and cart page, while also allowing to filter products by their designated category.",
         code: "https://github.com/BaileyKH/stoked-app",
-        demo: "https://stokedsnow.netlify.app/"
+        demo: "https://stokedsnow.vercel.app/"
     },
     {
         id: 2,
@@ -16,6 +16,6 @@ export const projects = [
         img: Jammming,
         description: "Utilizing the SpotifyAPI, Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account. ",
         code: "https://github.com/BaileyKH/Jammming",
-        demo: "https://playlistcreation.netlify.app/"
+        demo: "https://jammming-spotify.vercel.app/"
     },
 ]
