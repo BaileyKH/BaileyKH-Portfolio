51fd55565ed4e6adce50ba59c1f0b658dc8077e5
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-04 16:54:48 -0700

    hero responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ import img from '/src/assets/headshot.jpg'
 
 export const Hero = () => {
     return(
-        <div className="flex justify-center items-center h-screen mx-52 mt-12 px-10">
+        <div className="flex justify-center items-center h-screen md:mx-52 mt-12 px-10">
             <div className="flex">
-                <div className="w-2/3 flex flex-col mr-8">
+                <div className="w-full md:w-2/3 flex flex-col md:mr-8">
                     <h1 className="text-6xl pb-5 font-bold">Front-End React Developer</h1>
                     <p className="pb-8 text-lg font-light leading-relaxed">Hello! I'm Bailey Henderson and I am Front-End React <span className="line-through">addict</span> Developer currently based in Laveen, Arizona 🌵</p>
                     <div className="flex pb-6 justify-start items-center ">
@@ -17,8 +17,8 @@ export const Hero = () => {
                     </div>
                     <div className="flex items-start">
                     </div>
-                    <div className="mt-24 flex justify-start items-center">
-                        <h1 className="font-semibold mr-4">Stack |</h1>
+                    <div className="mt-14 md:mt-24 flex justify-start items-center">
+                        <h1 className="font-semibold mr-4">Stack <span className="hidden md:inline">|</span></h1>
                         <FontAwesomeIcon icon={faHtml5} style={{color: "#e54c26",}} className="icon"/>
                         <FontAwesomeIcon icon={faCss3Alt} style={{color: "#264de4",}} className="icon"/>
                         <FontAwesomeIcon icon={faJs} style={{color: "#f0db4f",}} className="icon"/>
@@ -26,7 +26,7 @@ export const Hero = () => {
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" className="h-[31.5px] w-[31.5px] icon"/>
                     </div>
                 </div>
-                <div className="w-1/3 flex justify-end items-start">
+                <div className="md:w-1/3 hidden md:flex justify-end items-start">
                     <img src={img} className="rounded-full border-4 border-primaryGray" alt="headshot of Bailey Henderson"/>
                 </div>
             </div>
