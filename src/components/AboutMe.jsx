a52e183e069144b134689d5536685830a5940282
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-04 16:58:12 -0700

    about me responsive pad update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,11 +4,11 @@ export const AboutMe = () => {
     return(
         <div className="bg-white">
             <div className="w-screen">
-                <div className="flex px-12 py-20 drop-shadow-lg">
-                    <div id="aboutme" className="w-1/2">
-                        <img src={img2} className="rounded-l-xl" alt="laptop sitting on desk"/>
+                <div className="flex flex-col md:flex-row md:px-12 md:py-20 drop-shadow-lg">
+                    <div id="aboutme" className="md:w-1/2">
+                        <img src={img2} className="md:rounded-l-xl" alt="laptop sitting on desk"/>
                     </div>
-                    <div className="w-1/2 flex flex-col justify-center items-start bg-primaryGray px-8 rounded-r-xl">
+                    <div className="md:w-1/2 flex flex-col justify-center items-start bg-primaryGray py-8 md:py-0 px-8 md:rounded-r-xl">
                         <h2 className="about-me text-lg text-primaryRed font-semibold pb-4">ABOUT ME</h2>
                         <h2 className="about-me font-bold">Front-End React Developer</h2>
                         <h2 className="about-me text-lg pb-8">Based in Laveen, Arizona 🌵</h2>
