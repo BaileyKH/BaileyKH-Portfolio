8bd723c9058a3d4e32034e8ef9b3ca2de0d3cf17
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-06 11:04:17 -0700

    adjusted hero headshot img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ export const Hero = () => {
                     </div>
                 </div>
                 <div className="md:w-1/3 hidden md:flex justify-end items-start">
-                    <img src={img} className="rounded-full border-4 border-primaryGray" alt="headshot of Bailey Henderson"/>
+                    <img src={img} className="rounded-full border-4 border-primaryGray w-[250px] h-[250px]" alt="headshot of Bailey Henderson"/>
                 </div>
             </div>
         </div>
