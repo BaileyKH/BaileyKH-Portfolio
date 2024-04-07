36ecec2bd9270f39f8fecc34b4b7e0ee06f55172
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-06 17:46:33 -0700

    new project section added

---
+++ src/components/NewProjectData.js
@@ -0,0 +1,11 @@
+import underdev from '/src/assets/underdev.avif'
+
+export const newprojects = [
+    {
+        id: 1,
+        title: "ELEVEN59",
+        img: underdev,
+        description: "ELEVEN59 will redefine collaborative learning with its sleek, user-friendly interface that will bring students together for real-time study sessions, discussions, and resource sharing. This front-end web application will integrate seamlessly with top-tier services for chat and scheduling, making it the perfect platform for learners to connect, share, and thrive academically. Soon you will be able to dive into ELEVEN59 and join a community where education meets innovation.",
+        code: "https://github.com/BaileyKH/stoked-app",
+    },
+]
