a87ecd517e4f17c3b19455fc3c502998c6744f23
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-06 17:49:13 -0700

    new project gh redirect fix

--- src/components/NewProjectData.js
+++ src/components/NewProjectData.js
@@ -6,6 +6,6 @@ export const newprojects = [
         title: "ELEVEN59",
         img: underdev,
         description: "ELEVEN59 will redefine collaborative learning with its sleek, user-friendly interface that will bring students together for real-time study sessions, discussions, and resource sharing. This front-end web application will integrate seamlessly with top-tier services for chat and scheduling, making it the perfect platform for learners to connect, share, and thrive academically. Soon you will be able to dive into ELEVEN59 and join a community where education meets innovation.",
-        code: "https://github.com/BaileyKH/stoked-app",
+        code: "https://github.com/BaileyKH/ELEVEN59",
     },
 ]
