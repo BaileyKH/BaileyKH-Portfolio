31312c510af9424de7846127e59dc63e785db2d1
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-06-11 10:09:26 -0700

    removal of mobile js projects

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -38,7 +38,7 @@ export const projects = [
         id: 4,
         title: "Jammming",
         img: Jammming,
-        description: "Utilizing the SpotifyAPI, Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account. ",
+        description: "Utilizing the SpotifyAPI, Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account.",
         code: "https://github.com/BaileyKH/Jammming",
         demo: "https://jammming-spotify.vercel.app/",
         structure: "React",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,6 @@ export const Home = () => {
             <AboutMe />
             <Projects />
             <NewProjects />
-            <Jsprojects />
             <ContactMe />
         </div>
     );
