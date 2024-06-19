7fe7b616c323865051b5d82390e270bba89d0a0c
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-06-19 13:27:03 -0700

    Projects | Nature Shot

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -1,3 +1,4 @@
+import natureShot from 'src/assets/nature-shot.png'
 import Unknown from '/src/assets/Unknown.png'
 import Quizzical from '/src/assets/Quizzical.png'
 import Stoked from '/src/assets/Stoked.avif'
@@ -6,6 +7,16 @@ import Jammming from '/src/assets/Jammming.avif'
 export const projects = [
     {
         id: 1,
+        title: "Nature Shot",
+        img: natureShot,
+        description: "Nature Shot is a sleek and responsive photography portfolio website built with React. This project showcases stunning nature photography from around the world, emphasizing clean design and seamless user experience. Explore the breathtaking gallery and immerse yourself in the beauty of nature. The website also features form validation on the contact page to ensure reliable communication.",
+        code: "https://github.com/BaileyKH/nature-shot",
+        demo: "https://natureshot.vercel.app/",
+        structure: "React",
+        style: "Tailwind CSS"
+    },
+    {
+        id: 2,
         title: "The Unknown",
         img: Unknown,
         description: "The Unknown is an innovative e-commerce platform specializing in camping equipment. Users can explore various product pages, detailed product descriptions, campsite information, and a shopping cart. The platform features advanced filtering options to categorize products effectively. Additionally, it integrates the NPS API, enabling users to search for campsites by their preferred state.",
@@ -15,7 +26,7 @@ export const projects = [
         style: "Tailwind CSS"
     },
     {
-        id: 2,
+        id: 3,
         title: "Quizzical",
         img: Quizzical,
         description: "Quizzical is an engaging quiz app that utilizes the Open Trivia Database API and allows users to test their knowledge with multiple-choice questions. Built with React, it features a sleek and responsive design, ensuring a seamless experience on any device. The app also provides instant feedback on answers, enhancing the learning experience.",
@@ -25,7 +36,7 @@ export const projects = [
         style: "CSS"
     },
     {
-        id: 3,
+        id: 4,
         title: "Stoked Snowboarding",
         img: Stoked,
         description: "Stoked is a simple ecommerce website that allows users to browse for snowboard equipment. It allows for views of a product page and cart page, while also allowing to filter products by their designated category.",
@@ -35,7 +46,7 @@ export const projects = [
         style: "CSS"
     },
     {
-        id: 4,
+        id: 5,
         title: "Jammming",
         img: Jammming,
         description: "Utilizing the SpotifyAPI, Jammming is a music website that allows a user to search for songs or a specific artist, then displays the most relevant results. After displaying the results, the user can then add thier choice of songs to a custom playlist, which can then be directly saved to their personal spotify account.",
