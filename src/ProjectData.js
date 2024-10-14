39ddd56ca88b668723eb4b1114ec3f7f284bc85a
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-13 17:18:45 -0700

    new project added: spooky bros

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,3 +1,4 @@
+import dutchBros from '/src/assets/projects/spooky-bros.png'
 import thrive from '/src/assets/projects/thrive.png'
 import natureShot from '/src/assets/projects/nature-shot.png'
 import Unknown from '/src/assets/projects/Unknown.png'
@@ -8,6 +9,16 @@ import Jammming from '/src/assets/projects/Jammming.avif'
 export const projects = [
     {
         id: 1,
+        title: "Spooky Bros",
+        img: dutchBros,
+        description: "My (spooky) interpretation of the Dutch Bros website",
+        code: "https://github.com/BaileyKH/DutchClone",
+        demo: "https://www.spookybros.vercel.app/",
+        structure: "React",
+        style: "Tailwind CSS"
+    },
+    {
+        id: 2,
         title: "Thrive Construction",
         img: thrive,
         description: "Freelance: I had the opportunity to redesign the website for THRIVE Construction. My focus was on creating a modern, user-friendly interface that enhances the user experience and effectively communicates the company's mission and services.",
@@ -17,7 +28,7 @@ export const projects = [
         style: "Tailwind CSS"
     },
     {
-        id: 2,
+        id: 3,
         title: "Nature Shot",
         img: natureShot,
         description: "Nature Shot is a sleek and responsive photography portfolio website built with React. This project showcases stunning nature photography, emphasizing clean design and seamless user experience. This website also features form validation on the contact page to ensure reliable communication.",
@@ -27,7 +38,7 @@ export const projects = [
         style: "Tailwind CSS"
     },
     {
-        id: 3,
+        id: 4,
         title: "The Unknown",
         img: Unknown,
         description: "The Unknown is an innovative e-commerce platform specializing in camping equipment. Users can explore various product pages, detailed product descriptions, campsite information, and a shopping cart. The platform features advanced filtering options to categorize products effectively. Additionally, it integrates the NPS API, enabling users to search for campsites by their preferred state.",
@@ -37,7 +48,7 @@ export const projects = [
         style: "Tailwind CSS"
     },
     {
-        id: 4,
+        id: 5,
         title: "Quizzical",
         img: Quizzical,
         description: "Quizzical is an engaging quiz app that utilizes the Open Trivia Database API and allows users to test their knowledge with multiple-choice questions. Built with React, it features a sleek and responsive design, ensuring a seamless experience on any device. The app also provides instant feedback on answers, enhancing the learning experience.",
@@ -47,7 +58,7 @@ export const projects = [
         style: "CSS"
     },
     {
-        id: 5,
+        id: 6,
         title: "Stoked Snowboarding",
         img: Stoked,
         description: "Stoked is a simple ecommerce website that allows users to browse for snowboard equipment. It allows for views of a product page and cart page, while also allowing to filter products by their designated category.",
